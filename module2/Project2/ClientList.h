9d87ba103c3c986b6c71c375337dceae70187805
Chris Joseph Gallo <chrisjg3@users.noreply.github.com>
2020-07-21 10:15:54 -0400

    Added Comments to ClientList Class

--- module2/Project2/ClientList.h
+++ module2/Project2/ClientList.h
@@ -20,12 +20,15 @@ class ClientList: public vector<Client*>
 {
     public:
 
+    // This is used to call the function for inputting from a file
     void inputFile(string);
 
+    // This is used to call the function for inputting from console
     void consoleInput();
 
     ~ClientList();
 
+    // This is for printing each Client ponter's Client Obj to console
     friend ostream &operator<<(ostream &out, ClientList &cl);
 };
 
